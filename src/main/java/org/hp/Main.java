646519fd99e4d4a5d05ab6c107260a42cb662b5a
Keyur Bhatt <keyur@supertax.in>
2019-10-31 18:07:38 +0530

    Actions: 1. Version migration to spring boot 2.x.x RELEASE

--- src/main/java/org/hp/Main.java
+++ src/main/java/org/hp/Main.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class Main extends SpringBootServletInitializer {
